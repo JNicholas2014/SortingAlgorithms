78f9ffeec14e2013b951fd54d2f0863c5c78975e
Jeff Nicholas <jeffery.m.nicholas@gmail.com>
2017-08-04 14:38:46 -0500

    Add files via upload

---
+++ SelectionSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortingAlgorithms
+{
+    public class SelectionSort
+    {
+        public  void sort<T> (T[] arr, int n) where T : IComparable
+        {
+          for(int index = 0; index < n - 1; index++)
+            {
+                int indexOfNextSmallest = getIndexOfSmallest(arr, index, n - 1);
+                swap(arr, index, indexOfNextSmallest);
+            }
+        }
+
+        public  int getIndexOfSmallest<T>(T[] arr, int first, int last) where T : IComparable
+        {
+            T: IComparable min = arr[first];
+            int indexOfMinimum = first;
+            for(int i = first + 1; i <= last; i++)
+            {
+                if (arr[i].CompareTo(min) < 0)
+                {
+                    min = arr[i];
+                    indexOfMinimum = i;
+                }
+            }
+            return indexOfMinimum;
+        }
+
+        public   void swap<T>(T[] arr, int i, int j) where T : IComparable
+        {
+            T temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
---
+++ SortDriver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class SortDriver
+    {
+        public static void Main(string[] args)
+        {
+            SelectionSort S = new SelectionSort();
+            Random ran = new Random();
+            int size = ran.Next(100);
+            Random generator = new Random();
+            int[] data = new int[size];
+            for (int i = 0; i < size - 1; i++)
+            {
+                data[i] = generator.Next(255);
+            }
+            Console.WriteLine("Data Created");
+            Console.WriteLine("The current data set is as follows: ");
+            Console.WriteLine();
+            for (int i = 0; i < size; i++) {
+                Console.Write(data[i] + " ");
+                if ((i % 10 == 0) && (i != 0))
+                    Console.WriteLine();
+            }   
+
+            S.sort(data, size);
+            Console.WriteLine();
+            Console.WriteLine("The sorted data set is as follows: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(data[i] + " ");
+                if ((i % 10 == 0) && (i != 0))
+                    Console.WriteLine();
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
